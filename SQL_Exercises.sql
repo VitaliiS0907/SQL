d9cbe5d087b3826b8d2b8f4c182afdec33340024
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-10-11 14:03:53 +0300

    SQL_Exercises

--- SQL_Exercises.sql
+++ SQL_Exercises.sql
@@ -1,5 +1,5 @@
-https://www.sql-ex.ru/
-/*Exercise: 1. Find the model number, speed and hard drive capacity for all the PCs with prices below $500. Result set: model, speed, hd/*
+--https://www.sql-ex.ru/
+--Exercise: 1. Find the model number, speed and hard drive capacity for all the PCs with prices below $500. Result set: model, speed, hd
 SELECT model, speed, hd
 FROM PC
 WHERE price < 500
