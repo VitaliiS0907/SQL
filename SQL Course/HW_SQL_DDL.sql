d0654ae2c14ef45ffd745a94c15b7e72dde0d684
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-11-08 22:02:33 +0200

    HW_SQL_DDL.sql

--- SQL Course/HW_SQL_DDL.sql
+++ SQL Course/HW_SQL_DDL.sql
@@ -66,3 +66,10 @@ create table salary(
 -- 4. Наполнить таблицу salary 15 строками.
 insert into salary(monthly_salary)
 values (1000),
+	(1100),
+	(1200),
+	(1300),
+	(1400),
+	(1500),
+	(1600),
+	(1700),
