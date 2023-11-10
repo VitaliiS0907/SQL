b9e9d7c88467980e3d097546be9af058c2a6181f
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-11-10 15:17:26 +0200

    HW_SQL_DDL.sql

--- SQL Course/HW_SQL_DDL.sql
+++ SQL Course/HW_SQL_DDL.sql
@@ -85,7 +85,7 @@ values (1000),
 -- 5. Создать таблицу employee_salary (id. Serial  primary key) + (employee_id. Int, not null, unique) + (salary_id. Int, not null)
 create table employee_salary(
     id serial  primary key,
-
+    employee_id int unique not null,
     
 -- 6. Наполнить таблицу employee_salary 40 строками (в 10 строк из 40 вставить несуществующие employee_id):
 insert into employee_salary(employee_id, salary_id)
