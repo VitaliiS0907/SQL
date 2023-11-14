6406e1c7350b6a083ae326998bed57935082bb4c
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-11-14 22:18:48 +0200

    HW_SQL_DDL.sql

--- SQL Course/HW_SQL_DDL.sql
+++ SQL Course/HW_SQL_DDL.sql
@@ -177,4 +177,9 @@ create table roles_employee(
 -- 11. Наполнить таблицу roles_employee 40 строками:
 insert into roles_employee(employee_id, role_id)
 values (1, 1),
+    (2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5),
+	(6, 6),
 
