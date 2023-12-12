1c0dda9573f1e819cc7486878e3f6fe3fd7e6edc
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-12 19:03:04 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -3,7 +3,7 @@ select employees.employee_name, salary.monthly_salary from employee_salary
 join employees
 on employee_salary.employee_id = employees.id
 join salary
-
+on employee_salary.salary_id
 -- 2. Вывести всех работников у которых ЗП меньше 2000.
 -- 3. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
 -- 4. Вывести все зарплатные позиции  меньше 2000 но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
