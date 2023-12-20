1805b6251794b1e8da25355c8db522f0d5438751
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-20 13:59:43 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -39,6 +39,7 @@ select employee_name, role_name from roles_employee
 join employees
 on roles_employee.employee_id = employees.id
 join roles
+on roles_employee.role_id =
 
 -- 7. Вывести имена и должность только Java разработчиков.
 -- 8. Вывести имена и должность только Python разработчиков.
