ea923afa04978db2a0897b02b43a94d8d598e106
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-22 20:51:24 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -50,7 +50,7 @@ on roles_employee.role_id = roles.id
 where roles.role_name like '%Java developer%';
 
 -- 8. Вывести имена и должность только Python разработчиков.
-select employee_name, role_name
+select employee_name, role_name from roles_employee
 
 -- 9. Вывести имена и должность всех QA инженеров.
 -- 10. Вывести имена и должность ручных QA инженеров.
