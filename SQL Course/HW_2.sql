8d59f52a0389ec571f24cbcaf05638cb6edde7bc
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-25 22:09:31 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -71,6 +71,7 @@ join employees
 on roles_employee.employee_id = employees.id 
 join roles
 on roles_employee.role_id = roles.id
+where roles.role_name
 
 -- 11. Вывести имена и должность автоматизаторов QA.
 -- 12. Вывести имена и зарплаты Junior специалистов.
