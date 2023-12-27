80e941337067e2f657425a8b701161c98b455b2d
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-27 20:10:25 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -79,7 +79,7 @@ join employees
 on roles_employee.employee_id = employees.id 
 join roles
 on roles_employee.role_id = roles.id
-where roles.role_name
+where roles.role_name like '%Auto% QA%';
 
 -- 12. Вывести имена и зарплаты Junior специалистов.
 -- 13. Вывести имена и зарплаты Middle специалистов.
