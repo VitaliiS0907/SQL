ffb973c61d9bb3fec6ac8087daca2e5e0dac19e1
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-27 20:11:28 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -85,6 +85,8 @@ where roles.role_name like '%Auto% QA%';
 select employee_name, salary.monthly_salary, roles.role_name from employees
 join employee_salary  
 on employees.id = employee_salary.employee_id 
+join salary 
+
 -- 13. Вывести имена и зарплаты Middle специалистов.
 -- 14. Вывести имена и зарплаты Senior специалистов.
 -- 15. Вывести зарплаты Java разработчиков.
