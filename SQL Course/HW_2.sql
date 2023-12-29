a555de978f8fb50bec1a770d96e74aa83762966e
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-29 18:07:21 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -100,7 +100,7 @@ on employees.id = employee_salary.employee_id
 join salary 
 on salary.id = employee_salary.salary_id
 join roles_employee 
-on employees.id =
+on employees.id = roles_employee.employee_id
 
 -- 14. Вывести имена и зарплаты Senior специалистов.
 -- 15. Вывести зарплаты Java разработчиков.
