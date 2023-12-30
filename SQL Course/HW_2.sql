2691827b26d528dfc7c2afd4956562dfa14a473b
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-30 13:13:14 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -123,6 +123,7 @@ join employee_salary
 on salary.id = employee_salary.salary_id
 join roles_employee 
 on roles_employee.employee_id = employee_salary.employee_id
+join roles 
 
 -- 16. Вывести зарплаты Python разработчиков.
 -- 17. Вывести имена и зарплаты Junior Python разработчиков.
