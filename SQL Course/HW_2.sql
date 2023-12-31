619fc99b70dfac60066145c5d9cd67d50c335370
Vitalii <vitaliy.shevchenko0907@gmail.com>
2023-12-31 15:43:19 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -125,7 +125,7 @@ join roles_employee
 on roles_employee.employee_id = employee_salary.employee_id
 join roles 
 on roles.id = roles_employee.role_id
-where roles.role_name like '%Java developer%'
+where roles.role_name like '%Java developer%';
 
 -- 16. Вывести зарплаты Python разработчиков.
 -- 17. Вывести имена и зарплаты Junior Python разработчиков.
