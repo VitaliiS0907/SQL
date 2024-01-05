de5cfb522ed2adc92b7f2b0a7369f2bc863ecb51
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-05 15:33:54 +0200

    HW_2

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -159,6 +159,8 @@ join roles_employee
 on employees.id = roles_employee.employee_id
 
 -- 19. Вывести имена и зарплаты Senior Java разработчиков.
+select employee_name,
+
 -- 20. Вывести зарплаты Junior QA инженеров.
 -- 21. Вывести среднюю зарплату всех Junior специалистов.
 -- 22. Вывести сумму зарплат JS разработчиков.
