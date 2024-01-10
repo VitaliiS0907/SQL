6f3b696f557d115ab9c02979b0cdff0ff42ea9c8
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-10 21:13:08 +0200

    HW_2

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -191,7 +191,7 @@ on roles.id = roles_employee.role_id
 where roles.role_name like '%Junior%';
 
 -- 22. Вывести сумму зарплат JS разработчиков.
-select round((salary.monthly_salary)) 
+select round((salary.monthly_salary)) from salary
 -- 23. Вывести минимальную зарплату QA инженеров.
 -- 24. Вывести максимальную зарплату QA инженеров.
 -- 25. Вывести количество QA инженеров.
