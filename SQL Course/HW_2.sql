582f3652e84aedaec6b955086b5b9401276a835a
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-10 21:12:12 +0200

    HW_2

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -187,7 +187,7 @@ on salary.id = employee_salary.salary_id
 join roles_employee 
 on roles_employee.employee_id = employee_salary.employee_id
 join roles 
-on roles.id = 
+on roles.id = roles_employee.role_id
 
 
 -- 22. Вывести сумму зарплат JS разработчиков.
