e835591cf4e88224e48de7bd15d91eb71f10dcbe
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-18 14:23:46 +0200

    HW_2

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -206,6 +206,7 @@ join roles_employee
 on roles_employee.employee_id = employee_salary.employee_id
 join roles 
 on roles.id = roles_employee.role_id
+where roles.role_name 
 
 -- 23. Вывести минимальную зарплату QA инженеров.
 -- 24. Вывести максимальную зарплату QA инженеров.
