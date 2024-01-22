1bc91c726995d95ab6ac713ec5fc486e114558af
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-22 21:28:47 +0200

    HW_2.sql

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -241,6 +241,7 @@ select count(roles.role_name) from roles_employee
 join employees 
 on roles_employee.employee_id = employees.id 
 join roles
+on roles_employee.employee_id = roles.id
 
 
 -- 27. Вывести количество разработчиков.
