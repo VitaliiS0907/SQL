4dd78a20b545920a8f6bbedea7648e99ba19d7ba
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-30 10:43:12 +0200

    HW_2

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -280,6 +280,7 @@ join roles_employee
 on employees.id = roles_employee.employee_id 
 join roles 
 on roles.id = roles_employee.role_id
+join employee_salary 
 
 -- 31. Вывести имена, должности и зарплату всех специалистов по возрастанию у специалистов, у которых зарплата меньше 2300.
 
