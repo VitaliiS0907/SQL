eef1873c41e09eceb6bbb08bef19e740cbf68d4a
Vitalii <vitaliy.shevchenko0907@gmail.com>
2024-01-31 12:08:11 +0200

    HW_2

--- SQL Course/HW_2.sql
+++ SQL Course/HW_2.sql
@@ -299,6 +299,7 @@ on employee_salary.employee_id = roles_employee.employee_id
 join salary
 on salary.id = employee_salary.salary_id
 where monthly_salary < 2300
+order by monthly_salary;
 
 -- 32. Вывести имена, должности и ЗП всех специалистов по возрастанию у специалистов, у которых зарплата равна 1100, 1500, 2000.
 
